b39dfb5ca3a9dd2699885154ea586a5258770bba
Claycuy <unggaslauncher@gmail.com>
2026-08-17 09:14:41 +0800

    chore: Setup prettier and eslint

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+import { Config } from 'prettier';
+
+const config: Config = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: 'lf',
+};
+
+export default config;
